87f6e878f52c0b0447a9739bfceb46682e1306b8
Dave Sroka <dave@davesroka.net>
2016-07-10 12:51:22 -0400

    Add package.json and webpack config from willowtree hiring project

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
   entry: `${APP_DIR}/js/main.js`,
   output: {
     path: BUILD_DIR,
-    publicPath: '/assets/',
+    publicPath: '/',
     filename: 'bundle.js',
   },
   module: {
@@ -22,11 +22,27 @@ module.exports = {
         test: /\.scss$/,
         loaders: ['style', 'css', 'sass'],
       },
+      {
+        test: /\.woff|woff2(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: 'url-loader?limit=10000&minetype=application/font-woff',
+      },
+      {
+        test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: 'file-loader',
+      },
     ],
   },
   resolve: {
     alias: {
+      containers: `${APP_DIR}/js/containers`,
       components: `${APP_DIR}/js/components`,
+      actions: `${APP_DIR}/js/actions`,
+      reducers: `${APP_DIR}/js/reducers`,
+      services: `${APP_DIR}/js/services`,
     },
   },
+  devServer: {
+    historyApiFallback: true,
+  },
+  devtool: 'source-map',
 };
